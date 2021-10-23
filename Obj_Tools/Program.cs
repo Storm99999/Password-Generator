de874e0e157174e1012955689756955e3e479c62
Storm <87811650+Storm99999@users.noreply.github.com>
2021-10-23 16:15:36 +0200

    Add files via upload

---
+++ Obj_Tools/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Obj_Tools
+{
+    class Program
+    {
+        // Most simple password gen
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LOG: What Length Should Your Password be?");
+            var inp = Console.ReadLine();
+            var input = Convert.ToInt32(inp);
+            Console.WriteLine(RandomString(input));
+            Console.ReadLine();
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#$äÖ?12345678910!§%&/()=";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
